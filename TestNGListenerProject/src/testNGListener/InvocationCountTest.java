2b45e809e40085080a4e35013c7da1542d4ae33e
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-06-14 18:47:34 +0300

    invocationCount

---
+++ TestNGListenerProject/src/testNGListener/InvocationCountTest.java
@@ -0,0 +1,22 @@
+package testNGListener;
+
+import org.testng.annotations.Test;
+
+public class InvocationCountTest {
+
+	@Test(invocationCount=10)
+	public void sum() {
+		int a = 10;
+		int b = 20;
+		int c= a+b;
+		System.out.println("Sum is===" + c);
+	}
+	
+//	@Test
+//	public void sum1() {
+//		int a = 10;
+//		int b = 20;
+//		int c= a+b;
+//		System.out.println("Sum is===" + c);
+//	}
+}
